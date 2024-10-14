fdca0c4b825296786c1bcfbb97650e7ec217e7e0
jkuleta <kuba.kuleta@onet.eu>
2024-10-14 12:06:58 +0200

    dupa

--- Delft_model.m
+++ Delft_model.m
@@ -5,8 +5,8 @@ visc_nu = 1.56*10^(-5);
 rho = 1.225;
 visc_mu = visc_nu*rho;
 H = 1;  
-N = 1001;
-dy = H/(N-1);
+N = 101;
+dy = H/(N-1); 
 
 %% Constants
 A_Plus = 26;
@@ -24,7 +24,7 @@ DNSData = LoadDNSData(filenames);
 %% Solver
 
 
-MESH = mesh(H, N);
+MESH = mesh_Delft(H, N);
 
 y = MESH.y;
 ddy = MESH.ddy;
@@ -70,7 +70,7 @@ k = 0.001*ones(N,1);
 epsilon = 0.001*ones(N,1);
 
 tolerance = 10^-6;
-error = norm(U - U_new, 2) / length(U);
+error = 1000;
 maxit = 20000;
 it =0;
 
@@ -87,64 +87,72 @@ visc_nu_eddy(end) = visc_nu_eddy(end-1);
 U = U_new;
 
 %Model loop
-while error > tolerance && it < 5
+for i=1:1000
+    fprintf('iteration number: %d.\n',i);
+    epsilon_old = epsilon;
+    k_old = k;
+    U_old = U;
+    visc_nu_eddy_oldb = visc_nu_eddy;
+
     k(k < 1e-6) = 1e-6;
     epsilon(epsilon < 1e-6) = 1e-6;
 
-    dUdy = ddy * U_new;
-
-
-    % dUdy = zeros(N,1);
-    % dUdy(1) = (U_new(2) - U_new(1))/dy;
-    % dUdy(end) = 0;
-    % for i=2:N-1
-    %     dUdy(i) = (U_new(i+1) - U_new(i-1))/(2*dy);
-    % end
-    % A_epsilon = ones(N);
-    % for i = 2:N-1
-    %     A_epsilon(i, i-1) = 4*sigma_epsilon*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);  % Coefficient U(y-dy)
-    %     A_epsilon (i, i) = -8*visc_nu*sigma_epsilon - 8*visc_nu_eddy(i);       % Coefficient U(y)
-    %     A_epsilon(i, i+1) =  4*sigma_epsilon*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);    % Coefficient U(y+dy)
-    % end
-    % 
-    % A_epsilon(1, 1) = 1;
-    % A_epsilon(end, end-1) = -1;
-    % A_epsilon(end, end) = 1;
+    dUdy = ddy * U;
 
 
     % Calculate Pk 
-    Pk = visc_nu_eddy .* dUdy^2;
+    Pk = visc_nu_eddy .* dUdy.^2;
+
+    %A-matrix for epsilon
+
+    visc_mu_tot = rho * (visc_nu + visc_nu_eddy/sigma_epsilon);
+
+    A_epsilon =   bsxfun(@times, visc_mu_tot, d2dy2) ... 
+        + bsxfun(@times, (ddy*visc_mu_tot), ddy);
+
+    A_epsilon(1,:) = 0;
+    A_epsilon(end, :) =0;
+    A_epsilon(1,1) = 1;
+    A_epsilon(end, end) = 1;
+    A_epsilon(end, end-1) = -1;
+
+    for index=2:N-1
+        A_epsilon(index,index) = A_epsilon(index,index) - C_2*rho*epsilon(index)/k(index);
+    end
+
 
     % Compute epsilon
-    f_epsilon = sigma_epsilon * (C_2.* epsilon.^2 ./ k - C_1 * epsilon .* Pk ./k);
+    B_epsilon = - C_1 * epsilon .* Pk ./k;
     
-    B_epsilon = 4*dy^2 .* f_epsilon;
     B_epsilon(1) = visc_nu *k(1) -2*k(2)+k(3)/dy^2;
     B_epsilon(end) = 0;
  
     
-    epsilon_old = epsilon;
+    
     epsilon = linsolve(A_epsilon, B_epsilon);
 
     % Compute k
-    A_k = ones(N);
-    for i = 2:N-1
-        A_k(i, i-1) = 4*sigma_k*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);  % Coefficient U(y-dy)
-        A_k(i, i) = -8*visc_nu*sigma_k - 8*visc_nu_eddy(i);       % Coefficient U(y)
-        A_k(i, i+1) =  4*sigma_k*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);    % Coefficient U(y+dy)
-    end
-    
-    A_k(1, 1) = 1;
-    A_k(end, end-1) = -1;
-    A_k(end, end) = 1;
+    %A-matrix for k
 
-    f_k = sigma_k .* (epsilon - Pk);
-    B_k = 4*dy^2 .*f_k;
+    visc_mu_tot = rho*(visc_nu + visc_nu_eddy/sigma_k);
+
+    A_k = bsxfun(@times, visc_mu_tot, d2dy2) ... 
+        + bsxfun(@times, (ddy*visc_mu_tot), ddy);
+    
+    A_k(1,:) = 0;
+    A_k(end, :) =0;
+    A_k(1,1) = 1;
+    A_k(end, end) = 1;
+    A_k(end, end-1) = -1;
+
+    for index=2:N-1
+        A_k(index,index) = A_k(index,index) - rho.*epsilon(index)./k(index);
+    end
+
+    B_k = - Pk;
     B_k(1) = 0;
     B_k(end) = 0;
     
-    k_old = k;
-    k = linsolve(A_k, B_k);
 
     %Calculate eddy viscosity 
 
@@ -165,11 +173,9 @@ while error > tolerance && it < 5
     A(1, 1) = 1;
     A(end, end-1) = -1;
     A(end, end) = 1;
-    U_new = linsolve(A, B);
+    U = linsolve(A, B);
 
-    error = norm(U - U_new, 2) / length(U);
-    U = U_new;
-    it = it+1;
+    error = norm(U_old - U, 2) / length(U);
 end
 
 figure(1);
--- test.m
+++ test.m
@@ -5,8 +5,6 @@ visc_nu = 1.56*10^(-5);
 rho = 1.225;
 visc_mu = visc_nu*rho;
 H = 1;  
-N = 1001;
-dy = H/(N-1);
 
 %% Constants
 A_Plus = 26;
@@ -22,13 +20,10 @@ DNSData = LoadDNSData(filenames);
 
 
 %% Solver
+dy = 0.001; 
+y = 0:dy:H;
+N = length(y);
 
-
-MESH = mesh(H, N);
-
-y = MESH.y;
-ddy = MESH.ddy;
-d2dy2 = MESH.d2dy2;
 
 visc_nu_eddy = 0 * ones(N,1);
 
@@ -36,6 +31,7 @@ Re_tau = Re_tau_values(1);
 DP_dx = -Re_tau^2*visc_nu^2/H^3;
 
 %Get laminar velocity
+N = length(y);
 U = ones(N,1);
 A = zeros(N,N);
 B = ones(N,1);
@@ -44,15 +40,19 @@ B = ((1./((visc_nu + visc_nu_eddy) .* rho)) .* DP_dx .* dy^2) .* B;
 %Boundary conditions
 B(1) = 0;
 B(end) = 0;
- 
-A = d2dy2;
-A(1, :) = 0;
-A(1,1) = 1;
-A(end, :)=0;
+    
+for i = 2:N-1
+        A(i, i-1) = 1;    % Coefficient U(y-dy)
+        A(i, i) = -2;       % Coefficient U(y)
+        A(i, i+1) = 1;     % Coefficient U(y+dy)
+end
+    
+A(1, 1) = 1;
 A(end, end-1) = -1;
 A(end, end) = 1;
     
 U_laminar = linsolve(A,B);
+
 
 %% k-epsilon model
 %constants
@@ -91,34 +91,33 @@ while error > tolerance && it < 5
     k(k < 1e-6) = 1e-6;
     epsilon(epsilon < 1e-6) = 1e-6;
 
-    dUdy = ddy * U_new;
-
-
-    % dUdy = zeros(N,1);
-    % dUdy(1) = (U_new(2) - U_new(1))/dy;
-    % dUdy(end) = 0;
-    % for i=2:N-1
-    %     dUdy(i) = (U_new(i+1) - U_new(i-1))/(2*dy);
-    % end
-    % A_epsilon = ones(N);
-    % for i = 2:N-1
-    %     A_epsilon(i, i-1) = 4*sigma_epsilon*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);  % Coefficient U(y-dy)
-    %     A_epsilon (i, i) = -8*visc_nu*sigma_epsilon - 8*visc_nu_eddy(i);       % Coefficient U(y)
-    %     A_epsilon(i, i+1) =  4*sigma_epsilon*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);    % Coefficient U(y+dy)
-    % end
-    % 
-    % A_epsilon(1, 1) = 1;
-    % A_epsilon(end, end-1) = -1;
-    % A_epsilon(end, end) = 1;
+    dUdy = zeros(N,1);
+    dUdy(1) = (U_new(2) - U_new(1))/dy;
+    dUdy(end) = 0;
+    for i=2:N-1
+        dUdy(i) = (U_new(i+1) - U_new(i-1))/(2*dy);
+    end
+    
+    A_epsilon = ones(N);
+    for i = 2:N-1
+        A_epsilon(i, i-1) = 2*visc_nu + visc_nu_eddy(i) + visc_nu_eddy(i-1);
+        A_epsilon(i,i) = -4*visc_nu - visc_nu_eddy(i+1) -visc_nu_eddy(i) - visc_nu_eddy(i-1);
+        A_epsilon(i,i+1) = 2*visc_nu + visc_nu_eddy(i) + visc_nu_eddy(i+1);
+    end
+    
+    A_epsilon(1, 1) = 1;
+    A_epsilon(end, end-1) = -1;
+    A_epsilon(end, end) = 1;
 
 
     % Calculate Pk 
-    Pk = visc_nu_eddy .* dUdy^2;
+    Pk = visc_nu_eddy .* dUdy.^2;
+
 
     % Compute epsilon
     f_epsilon = sigma_epsilon * (C_2.* epsilon.^2 ./ k - C_1 * epsilon .* Pk ./k);
     
-    B_epsilon = 4*dy^2 .* f_epsilon;
+    B_epsilon = 2*dy^2 .* f_epsilon;
     B_epsilon(1) = visc_nu *k(1) -2*k(2)+k(3)/dy^2;
     B_epsilon(end) = 0;
  
@@ -129,9 +128,9 @@ while error > tolerance && it < 5
     % Compute k
     A_k = ones(N);
     for i = 2:N-1
-        A_k(i, i-1) = 4*sigma_k*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);  % Coefficient U(y-dy)
-        A_k(i, i) = -8*visc_nu*sigma_k - 8*visc_nu_eddy(i);       % Coefficient U(y)
-        A_k(i, i+1) =  4*sigma_k*visc_nu + 4*visc_nu_eddy(i) + visc_nu_eddy (i-1) + visc_nu_eddy (i+1);    % Coefficient U(y+dy)
+        A_k(i, i-1) = 2*visc_nu + visc_nu_eddy(i) + visc_nu_eddy(i-1);
+        A_k(i,i) = -4*visc_nu - visc_nu_eddy(i+1) -visc_nu_eddy(i) - visc_nu_eddy(i-1);
+        A_k(i,i+1) = 2*visc_nu + visc_nu_eddy(i) + visc_nu_eddy(i+1);
     end
     
     A_k(1, 1) = 1;
@@ -139,7 +138,7 @@ while error > tolerance && it < 5
     A_k(end, end) = 1;
 
     f_k = sigma_k .* (epsilon - Pk);
-    B_k = 4*dy^2 .*f_k;
+    B_k = 2*dy^2 .*f_k;
     B_k(1) = 0;
     B_k(end) = 0;
     
@@ -155,16 +154,17 @@ while error > tolerance && it < 5
     B = 1/rho .*DP_dx*ones(N,1);
     B(1) = 0;
     B(end) = 0;
-       
-    for i = 2:N-1
-            A(i, i-1) = 1;    % Coefficient U(y-dy)
-            A(i, i) = -2;       % Coefficient U(y)
-            A(i, i+1) = 1;     % Coefficient U(y+dy)
-    end
-        
-    A(1, 1) = 1;
-    A(end, end-1) = -1;
-    A(end, end) = 1;
+    % 
+    % for i = 2:N-1
+    %         A(i, i-1) = 1;    % Coefficient U(y-dy)
+    %         A(i, i) = -2;       % Coefficient U(y)
+    %         A(i, i+1) = 1;     % Coefficient U(y+dy)
+    % end
+    % 
+    % A(1, 1) = 1;
+    % A(end, end-1) = -1;
+    % A(end, end) = 1;
+    A = A_k;
     U_new = linsolve(A, B);
 
     error = norm(U - U_new, 2) / length(U);
